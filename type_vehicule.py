e0ecfdbfd094e19089aba9621dcb1176498a0775
louisebarbet <barbet.louise50@gmail.com>
2024-04-02 16:47:49 +0200

    modif

--- type_vehicule.py
+++ type_vehicule.py
@@ -1,5 +1,9 @@
 class Type_de_Vehicule:
+    """ Décrit le véhicule.
+    Parameters
+    ----------
+    categorie: str
+        Catégorie du véhicule.
+    """
     def __init__(self) -> None:
-        """ fon,ction
-        """
         pass
