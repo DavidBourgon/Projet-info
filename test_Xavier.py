c6e99579234ec743c81f48fb95754e7317e8345f
xlecureuil <braquaval.xavier@gmail.com>
2024-04-09 15:22:22 +0200

    mort total

--- test_Xavier.py
+++ test_Xavier.py
@@ -1,3 +1,11 @@
 import panda as pd
 
-def
+# Charger le fichier Excel dans un DataFrame
+data = pd.read_excel("BdD_Bronx.xlsx")
+
+
+def nmbr_mort_total():
+    # Calculer le nombre total de personnes tuées
+    total_persons_killed = data["NUMBER.OF.PERSONS.KILLED"].sum()
+    return ["Nombre total de personnes tuées dans toute la base de données :",
+            total_persons_killed]
