a26ecd7b6ad8cd59cbeb52da65837d88d8c00d85
louisebarbet <barbet.louise50@gmail.com>
2024-04-09 16:22:39 +0200

    modif louise

--- secteur_public.py
+++ secteur_public.py
@@ -6,6 +6,9 @@ class SecteurPublic:
         self.pieton = pieton
         self.velo = velo
         self.vehicule = vehicule
+        if not isinstance(vehicule, TypeDeVehicule):
+            raise TypeError("Le véhicule doit être une instance de "
+            "TypeDeVehicule.")
 
     def calculer_mortalité_rue(rue: str):
         pass
