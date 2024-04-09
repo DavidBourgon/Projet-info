6ba4cba6c1348f58c9e9dba013ca52daa4fc63fb
xlecureuil <braquaval.xavier@gmail.com>
2024-04-09 16:24:35 +0200

    marche mort total

--- bordel_xavier.py
+++ bordel_xavier.py
@@ -12,3 +12,15 @@ def nmbr_mort_total(data):
     total_persons_killed = data["NUMBER.OF.PERSONS.KILLED"].sum()
     return ("Nombre total de personnes tuées dans toute la base de données :",
             total_persons_killed)
+
+
+# result = nmbr_mort_total(data)
+# print(result)
+
+def nmbr_mort_total_rue(data, street : str):
+    # Calculer le nombre total de personnes par rue
+    total_persons_killed = data.groupby(street)["NUMBER.OF.PERSONS.KILLED"].sum()
+    return ("Nombre total de personnes tuées dans toute la base de données :",
+            total_persons_killed)
+
+street = 
