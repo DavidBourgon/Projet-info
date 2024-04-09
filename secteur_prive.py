28a093c249b74a3de680da2d8d0741ae27d43849
DavidBourgon <david.bourgon@orange.fr>
2024-04-09 16:47:09 +0200

    maj David

--- secteur_prive.py
+++ secteur_prive.py
@@ -15,10 +15,21 @@ class SecteurPrive:
         pass
 
     def __calculer_risque_rue(self, vehicule, rue: str) -> float:
+        if not isinstance(rue, str):
+            raise TypeError("La rue doit être une instance de str.")
+        if not isinstance(vehicule, TypeDeVehicule):
+            raise TypeError("Le nom du vehicule n'est pas correct.")
         pass
 
     def __donner_prix(self, localisation: list[str], vehicule) -> float:
-        pass
+        if not isinstance(localisation, list):
+            raise TypeError("La localisation doit être une liste.")
+            indicateur_risque = 0
+            for k in len(localisation):
+                indicateur_risque = (
+                    indicateur_risque +
+                    self.__calculer_risque_rue(vehicule, localisation[k])
+                    )
 
     def __repr__(self, localisation: list[str], vehicule) -> str:
         return (f"Pour assurer votre véhicule de type, vous devez vous"
