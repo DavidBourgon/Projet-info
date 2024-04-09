42d280010eb38c60ac7aa28cc228b75d45b45110
louisebarbet <barbet.louise50@gmail.com>
2024-04-09 14:44:07 +0200

    public

--- secteur_public.py
+++ secteur_public.py
@@ -2,8 +2,13 @@ import pandas as pd
 
 
 class SecteurPublic:
-    def __init__(pieton, velo, vehicule):
+    def __init__(self, pieton, velo, vehicule):
+        self.pieton = pieton
+        self.velo = velo
+        self.vehicule = vehicule
+
+    def calculer_mortalité_rue(rue: str):
         pass
 
-    def donner_prix(localisation: list[str]):
+    def __localiser_infrastructure(zones: dict[int: list[str]]):
         pass
