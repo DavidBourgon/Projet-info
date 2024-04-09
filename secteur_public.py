e3b640963986b3855a5f9f7b53b447c8fb495e99
louisebarbet <barbet.louise50@gmail.com>
2024-04-09 16:43:08 +0200

    modif public louise

--- secteur_public.py
+++ secteur_public.py
@@ -18,7 +18,9 @@ class SecteurPublic:
 
     vehicule : bool
         indique si l'utilisateur concerné est un conducteur ou non
+
     """
+
     def __init__(self, pieton, velo, vehicule):
         self.pieton = pieton
         self.velo = velo
@@ -27,8 +29,21 @@ class SecteurPublic:
             raise TypeError("Le véhicule doit être une instance de "
                             "TypeDeVehicule.")
 
-    def calculer_mortalité_rue(rue: str):
-        return bx.nmbr_mort_total(data)/bx.nmbr_mort_total_rue(data)
+    def calculer_mortalité_rue(self, rue: str) -> int:
+        """ Calcule le taux de mortalité d'une rue.
 
-    def __localiser_infrastructure(zones: dict[int: list[str]]):
+        Parameters
+        ----------
+        rue : str
+            Nom de la rue dont on veut calculer le taux de mortalité
+
+        Returns
+        -------
+        str : taux de mortalité de la rue demandée
+
+        """
+        taux_mortalité = bx.nmbr_mort_total(data)/bx.nmbr_mort_total_rue(data)
+        return f"Le taux de mortalité de la rue {rue} est {taux_mortalité}."
+
+    def __localiser_infrastructure(self, zones: dict[int: list[str]]):
         pass
