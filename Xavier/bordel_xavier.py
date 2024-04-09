fd41b80e7a8d022b2adea53e1c07af8ce61fa3d0
xlecureuil <braquaval.xavier@gmail.com>
2024-04-09 19:20:46 +0200

    modif post ajout de la base

--- Xavier/bordel_xavier.py
+++ Xavier/bordel_xavier.py
@@ -4,7 +4,7 @@
 import pandas as pd
 
 
-data = pd.read_excel("bronx_2.xlsx")
+data = pd.read_excel("Bronx_2.xlsx")
 
 
 def nmbr_mort_total(data):
@@ -13,20 +13,20 @@ def nmbr_mort_total(data):
     return ("Nombre total de personnes tuées dans toute la base de données :",
             total_persons_killed)
 
-# result = nmbr_mort_total(data)
-# print(result)
+result = nmbr_mort_total(data)
+print(result)
 
 
-def nmbr_mort_total_rue(data, street: str):
-    # Calculer le nombre total de personnes par rue
-    street_data = data[data["CROSS.STREET.NAME"].str.contains(street) |
-                       data["ON.STREET.NAME"].str.contains(street) |
-                       data["OFF.STREET.NAME"].str.contains(street)]
-    total_persons_killed = street_data["NUMBER.OF.PERSONS.KILLED"].sum()
-    return ("Nombre total de personnes tuées dans la rue base de données :",
-            total_persons_killed)
+# def nmbr_mort_total_rue(data, street: str):
+#     # Calculer le nombre total de personnes par rue
+#     street_data = data[data["CROSS.STREET.NAME"].str.contains(street) |
+#                        data["ON.STREET.NAME"].str.contains(street) |
+#                        data["OFF.STREET.NAME"].str.contains(street)]
+#     total_persons_killed = street_data["NUMBER.OF.PERSONS.KILLED"].sum()
+#     return ("Nombre total de personnes tuées dans la rue base de données :",
+#             total_persons_killed)
 
 
-data = pd.read_excel("bronx_2.xlsx")
-street = 'HEATH AVENUE'
-print(nmbr_mort_total_rue(data, street))
+# data = pd.read_excel("Bronx_2.xlsx")
+# street = 'HEATH AVENUE'
+# print(nmbr_mort_total_rue(data, street))
