0af9da876f1d236b32ffcc07dabd2afe44d01cf3
xlecureuil <braquaval.xavier@gmail.com>
2024-04-09 19:25:42 +0200

    ça marche enfin

--- Xavier/bordel_xavier.py
+++ Xavier/bordel_xavier.py
@@ -14,18 +14,18 @@ def nmbr_mort_total(data):
             total_persons_killed)
 
 
-result = nmbr_mort_total(data)
-print(result)
+# result = nmbr_mort_total(data)
+# print(result)
 
 
-# def nmbr_mort_total_rue(data, street: str):
-#     # Calculer le nombre total de personnes par rue
-#     street_data = data[data["CROSS.STREET.NAME"].str.contains(street) |
-#                        data["ON.STREET.NAME"].str.contains(street) |
-#                        data["OFF.STREET.NAME"].str.contains(street)]
-#     total_persons_killed = street_data["NUMBER.OF.PERSONS.KILLED"].sum()
-#     return ("Nombre total de personnes tuées dans la rue base de données :",
-#             total_persons_killed)
+def nmbr_mort_total_rue(data, street: str):
+    # Calculer le nombre total de personnes par rue
+    street_data = data[data["CROSS.STREET.NAME"].str.contains(street) |
+                       data["ON.STREET.NAME"].str.contains(street) |
+                       data["OFF.STREET.NAME"].str.contains(street)]
+    total_persons_killed = street_data["NUMBER.OF.PERSONS.KILLED"].sum()
+    return ("Nombre total de personnes tuées dans la rue base de données :",
+            total_persons_killed)
 
 
 # data = pd.read_excel("Bronx_2.xlsx")
