df32431ba5944ddc45eefd0b4824161d36d9f8d6
louisebarbet <barbet.louise50@gmail.com>
2024-04-25 12:38:16 +0200

    modif louise docstrings

--- type_vehicule.py
+++ type_vehicule.py
@@ -1,13 +1,13 @@
 class TypeDeVehicule:
-    """ Décrit le véhicule.
+    """
+    Décrit le véhicule.
+
     Parameters
     ----------
     categorie: str
         Catégorie du véhicule.
     """
     def __init__(self, categorie: str) -> None:
-        """ fonction
-        """
+
         if not isinstance(categorie, str):
             raise TypeError("Le nom doit être une instance de str.")
-
