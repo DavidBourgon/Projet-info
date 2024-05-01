c46db018bd050d2b83be5af9c72db778da709075
xlecureuil <braquaval.xavier@gmail.com>
2024-05-01 20:24:42 +0200

    test_utilisateur

--- test_utilisateur.py
+++ test_utilisateur.py
@@ -5,6 +5,7 @@ import re
 
 data = pd.read_excel("Bronx_2.xlsx")
 
+
 # Définition des cas de test pour nombre_observation échec
 @pytest.mark.parametrize(
     'kwargs, erreur, message_erreur',
@@ -21,49 +22,31 @@ def test_nombre_observation_echec(kwargs,
         Utilisateur.nombre_observation(kwargs)
 
 
-# Définition des cas de test pour nombre_observation réussie
-@pytest.mark.parametrize(
-    'kwargs_str',
-    [
-        'station_kwargs'
-    ]
-)
-def test_station_nombre_observation_succes(kwargs_str, request):
-    kwargs = request.getfixturevalue(kwargs_str)
-    Utilisateur.nombre_observation(kwargs)
-
-# # BROUILLON NE PAS TOUCHER !!!
-
-# # Définition des cas de test pour la méthode nombre_observation échec
+# # Définition des cas de test pour nombre_observation réussie
 # @pytest.mark.parametrize(
-#     'kwargs, data, erreur, message_erreur',
+#     'kwargs_str',
 #     [
-#         ({}, 60, TypeError,
+#         'station_kwargs'
+#     ]
+# )
+# def test_station_nombre_observation_succes(kwargs_str, request):
+#     kwargs = request.getfixturevalue(kwargs_str)
+#     Utilisateur.nombre_observation(kwargs)
+
+# Définition des cas de test pour calcul_totaux_statut échec
+
+# @pytest.mark.parametrize(
+#     'kwargs, erreur, message_erreur',
+#     [
+#         ({2, "str"}, TypeError,
 #          "La base de données doit être un DataFrame.")
+
 #     ]
 # )
-# def test_nombre_observation_echec(obs_kwargs, kwargs, data,
-#                                   erreur, message_erreur):
-#     obs_kwargs.update(kwargs)
-#     utilisateur = Utilisateur(**obs_kwargs)
+# def test_calcul_totaux_statut_echec(utilisateur_kwargs,
+#                                     kwargs,
+#                                     erreur,
+#                                     message_erreur):
+#     utilisateur_kwargs.update(kwargs)
 #     with pytest.raises(erreur, match=re.escape(message_erreur)):
-#         utilisateur.nombre_observation(data)
-
-
-# # Définition des cas de test pour la méthode nombre_observation succés
-# @pytest.mark.parametrize(
-#     'kwargs, nom_carburant, erreur, message_erreur',
-#     [
-#         ({}, 60, TypeError,
-#          "Le nom de carburant doit être une chaîne de caractères."),
-#         ({"pompes": {"SP98": pytest.pompe_gpl},
-#           "prix": {"SP98": 1.5}}, 'SP98', ValueError,
-#          "Le carburant ne correspond pas au nom de la pompe"),
-#     ]
-# )
-# def test_station_verif_pompe(station_kwargs, kwargs, nom_carburant,
-#                              erreur, message_erreur):
-#     station_kwargs.update(kwargs)
-#     station = Station(**station_kwargs)
-#     with pytest.raises(erreur, match=re.escape(message_erreur)):
-#         station._Station__verifier_pompe(nom_carburant)
+#         Utilisateur(**utilisateur_kwargs)
