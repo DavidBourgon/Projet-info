7b42a10a0d0b5dadd3f59e494b1145e4597ead3b
DavidBourgon <david.bourgon@orange.fr>
2024-05-03 18:03:01 +0200

    application

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from particulier import Particulier
+
+app = Flask(__name__)
+
+@app.route('/votre-endpoint-python', methods=['POST'])
+def votre_fonction_python():
+    data = request.json
+    adresse_de_depart = data.get('start')
+    adresse_d_arrivee = data.get('end')
+    moyen_de_locomotion = data.get('vehicle')
+    Personne = Particulier(moyen_de_locomotion)
+    results = Personne.eviter_zone_risquee(adresse_de_depart, adresse_d_arrivee)
+    # Renvoyer les résultats au format JSON
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
