9909cd30bd5b7694fffcf672e95a9afc9e8cbbce
xlecureuil <braquaval.xavier@gmail.com>
2024-05-05 23:55:18 +0200

    secteur_prive

--- secteur_prive.py
+++ secteur_prive.py
@@ -1,4 +1,6 @@
 from Utilisateur import Utilisateur
+from geopy.geocoders import Nominatim
+from pyroutelib3 import Router
 
 
 class SecteurPrive:
@@ -29,7 +31,7 @@ class SecteurPrive:
         self.nom_assureur = nom_assureur
         self.marge = marge
 
-    def __decompose_trajet(adresse_depart, adresse_arrrive):
+    def __decompose_trajet(self, adresse_depart, adresse_arrivee):
         """
         Décompose un trajet entre 2 adresses.
 
@@ -47,9 +49,32 @@ class SecteurPrive:
             liste des rues qu'il faut emprunter pour réaliser le trajet
             entre les 2 adresses données en entrée.
         """
-        pass
+        # Coordonnées des 2 adresses
+        geolocator = Nominatim(user_agent="carte_bronx")
+        localisation_1 = geolocator.geocode(adresse_depart)
+        coord_depart = (localisation_1.latitude, localisation_1.longitude)
+        localisation_2 = geolocator.geocode(adresse_arrivee)
+        coord_arrivee = (localisation_2.latitude, localisation_2.longitude)
 
-    def __donner_prix(self, data, localisation, categorie):
+        # Création de l'itinéraire
+        router = Router(self.categorie)
+        node_depart = router.findNode(*coord_depart)
+        node_arrivee = router.findNode(*coord_arrivee)
+        status, route = router.doRoute(node_depart, node_arrivee)
+
+        # Récupération des noms des rues
+        rues = []
+        for node in route:
+            coordonnees = router.nodeLatLon(node)
+            localisation = geolocator.reverse(coordonnees)
+            adresse = localisation.raw['address']
+            nom_rue = adresse.get('road', None)
+            if nom_rue:
+                rues.append(nom_rue)
+
+        return rues
+
+    def __donner_prix(self, data, rues, categorie):
         """
         Détermine le prix qu'un client va payer en fonction de comment
         il se déplace sur son trajet le plus fréquent
@@ -68,18 +93,18 @@ class SecteurPrive:
             Prix que va devoir payer le client chez cet assureur.
 
         """
-        if not isinstance(localisation, list):
-            raise TypeError("La localisation doit être une liste de rues")
+        if not isinstance(rues, list):
+            raise TypeError("L'argument rues doit être une liste de rues")
 
         ind_risque = 0
-        for k in len(localisation):
+        for k in len(rues):
             ind_risque += Utilisateur.risque_rue(data,
-                                                 localisation[k],
+                                                 rues[k],
                                                  categorie)
         prix = (400 * ind_risque) * (1 + self.marge)
         return prix
 
-    def __repr__(self, localisation: list[str], categorie):
+    def __repr__(self, rues: list[str], categorie):
         """
 
         Représentation officielle de la réponse de l'assureur au client
@@ -88,4 +113,4 @@ class SecteurPrive:
         """
         return (f"Pour vous assurer sur votre trajet quotidien,"
                 f" vous devez vous acquitter de"
-                f" {self.__donner_prix(localisation, categorie)}")
+                f" {self.__donner_prix(rues, categorie)}")
