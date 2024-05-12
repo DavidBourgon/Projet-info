bce15dcbb512de78c3ac2fbed59b2dd2de848052
xlecureuil <braquaval.xavier@gmail.com>
2024-05-12 22:42:46 +0200

    main2

--- main2.py
+++ main2.py
@@ -13,23 +13,22 @@ Axa = SecteurPrive("AXA", 0.8)
 # pour son trajet le plus fréquent
 # Si ce dernier se déplace en voiture ainsi que le type de véhicule
 # Chez Groupama
-print(Groupama.__repr__(data, 
+print(Groupama.__repr__(data, "1818 Archer St, Bronx, NY",
                         "1794 Merrill Street, Bronx, NY",
-                        "1928 Benedict Ave, Bronx, NY",
                         "car", "Pick-up Truck"))
 
 # Chez Axa
-print(Axa.__repr__(data,"1794 Merrill Street, Bronx, NY",
-                        "1928 Benedict Ave, Bronx, NY",
+print(Axa.__repr__(data,"1818 Archer St, Bronx, NY",
+                        "1794 Merrill Street, Bronx, NY",
                         "car", "Pick-up Truck"))
 
 # Lorsque le client se déplace en vélo
 # Chez Groupama
-print(Groupama.__repr__(data, "1794 Merrill Street, Bronx, NY",
-                        "1928 Benedict Ave, Bronx, NY",
+print(Groupama.__repr__(data, "1818 Archer St, Bronx, NY",
+                        "1794 Merrill Street, Bronx, NY",
                         "cycle"))
 
 # Chez Axa
-print(Axa.__repr__(data, "1794 Merrill Street, Bronx, NY",
-                   "1928 Benedict Ave, Bronx, NY",
+print(Axa.__repr__(data, "1818 Archer St, Bronx, NY",
+                   "1794 Merrill Street, Bronx, NY",
                    "cycle"))
